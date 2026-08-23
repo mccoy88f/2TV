7c7e29f7e2d18f04c91624cc95d02ddb187123fe
Antonello <antonellomigliorelli@gmail.com>
2026-08-23 21:49:55 +0200

    Add raster 16:9 PNG banner for Android TV Leanback launcher tile

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -14,8 +14,9 @@ android {
         applicationId = "com.twotv.app"
         minSdk = 26
         targetSdk = 35
-        versionCode = 4
-        versionName = "1.0.3"
+        versionCode = 5
+        versionName = "1.0.4"
+
 
 
 
